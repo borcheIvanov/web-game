3961c89060797398d6ead455cac51460421d1471
Borche Ivanov <borche.ivanov@outlook.com>
2020-02-09 21:31:40 +0100

    test project added to solution

--- web-game/Controllers/GameController.cs
+++ web-game/Controllers/GameController.cs
@@ -11,11 +11,12 @@ namespace web_game.Controllers
     [ApiController]
     public class GameController : ControllerBase
     {
+        private readonly IGameService _gameService;
         private readonly IMatchesService _matchesService;
         private readonly UserManager<ApplicationUser> _userManager;
-        private readonly IGameService _gameService;
 
-        public GameController(IMatchesService matchesService, UserManager<ApplicationUser> userManager, IGameService gameService)
+        public GameController(IMatchesService matchesService, UserManager<ApplicationUser> userManager,
+            IGameService gameService)
         {
             _userManager = userManager;
             _gameService = gameService;
@@ -32,23 +33,22 @@ namespace web_game.Controllers
         [HttpGet]
         public IActionResult GetRandomNumber()
         {
-            var randomNumber = new Random().Next(0, 100); 
-            return Ok(new{randomNumber});
+            var randomNumber = new Random().Next(0, 100);
+            return Ok(new {randomNumber});
         }
 
         [HttpPost]
         public async Task<IActionResult> Submit([FromBody] SubmitViewModel submitted)
         {
-
             var user = await GetCurrentUserAsync();
-            
+
             if (await _gameService.HasUserSubmitted(user.Id))
                 return BadRequest();
 
 
             var match = await _matchesService.GetCurrentMatch();
-            
-            var game = new Game()
+
+            var game = new Game
             {
                 MatchId = match.Id,
                 UserId = user.Id,
@@ -58,11 +58,10 @@ namespace web_game.Controllers
             _gameService.SubmitGame(game);
             return Ok();
         }
-        
-        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
-        
-        
+
+        private Task<ApplicationUser> GetCurrentUserAsync()
+        {
+            return _userManager.GetUserAsync(HttpContext.User);
+        }
     }
-    
-    
 }
