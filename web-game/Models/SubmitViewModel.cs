285a5a637e7cbbfd9f79434797dabaf159b1f6b7
Borche Ivanov <borche.ivanov@outlook.com>
2020-03-26 20:58:39 +0100

    unused code removed

--- web-game/Models/SubmitViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace web_game.Models
-{
-    public class SubmitViewModel
-    {
-        [Range(0, 100)] public int Number { get; set; }
-    }
-}
