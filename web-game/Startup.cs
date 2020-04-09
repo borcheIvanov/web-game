51d19b013065ab088f2fbb2e7dc80adf79a5f2fe
Borche Ivanov <borche.ivanov@outlook.com>
2020-04-09 21:59:50 +0200

    auth and openapi documentation implemented

--- web-game/Startup.cs
+++ web-game/Startup.cs
@@ -1,11 +1,14 @@
-using Microsoft.AspNetCore.Authentication;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using web_game.Data;
+using NSwag;
+using NSwag.AspNetCore;
+using NSwag.Generation.Processors.Security;
 using web_game.Models;
 using web_game.Repositories;
 using web_game.Services;
@@ -24,20 +27,51 @@ namespace web_game
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllers();
 
-            services.AddDefaultIdentity<ApplicationUser>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            var authConfig = new AuthConfig();
+            Configuration.Bind("AzureAd", authConfig);
 
-            services.AddIdentityServer()
-                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+            services.AddAuthentication(options => options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme)
+                .AddMicrosoftAccount(microsoftOptions =>
+                {
+                    microsoftOptions.ClientId = authConfig.ClientId;
+                    microsoftOptions.ClientSecret = authConfig.ClientSecret;
+                })
+                .AddJwtBearer(opt =>
+                {
+                    opt.Audience = authConfig.ClientId;
+                    opt.Authority = $"{authConfig.Instance}{authConfig.TenantId}";
+                });
+
 
             services.AddScoped<IMatchRepository, MatchRepository>();
+            services.AddScoped<IRepository<Game>, Repository<Game>>();
             services.AddTransient<IMatchesService, MatchesService>();
 
-            services.AddAuthentication()
-                .AddIdentityServerJwt();
+            services.AddOpenApiDocument((options, s) =>
+            {
+                options.Title = "Web Game";
+                options.Version = "v1";
+
+                options.AddSecurity("oauth2", Enumerable.Empty<string>(), new OpenApiSecurityScheme
+                {
+                    AuthorizationUrl = $"{authConfig.Authority}/oauth2/v2.0/authorize",
+                    Flow = OpenApiOAuth2Flow.Implicit,
+                    TokenUrl = $"{authConfig.Authority}/oauth2/v2.0/token",
+                    // "https://login.microsoftonline.com/eb185d82-855f-4910-91b4-36603df8fce9/oauth2/v2.0/token",
+                    Type = OpenApiSecuritySchemeType.OAuth2,
+                    Scopes = new Dictionary<string, string>
+                    {
+                        {
+                            "openid", "open Id"
+                        }
+                    },
+                    Scheme = JwtBearerDefaults.AuthenticationScheme
+                });
+            
+                options.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("oauth2"));
+            });
 
             services.AddControllers();
         }
@@ -45,13 +79,11 @@ namespace web_game
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
             }
-            else
-            {
+            else {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios,
                 // see https://aka.ms/aspnetcore-hsts.
@@ -62,9 +94,24 @@ namespace web_game
 
             app.UseRouting();
 
+            var authConfig = new AuthConfig();
+            Configuration.Bind("AzureAd", authConfig);
+
+            app.UseOpenApi();
+            
+            app.UseSwaggerUi3(options =>
+            {
+                options.OAuth2Client = new OAuth2ClientSettings
+                {
+                    ClientId = authConfig.ClientId,
+                    AppName = "Web Game",
+                    ClientSecret = authConfig.ClientSecret
+                };
+            });
+      
             app.UseAuthentication();
-            app.UseIdentityServer();
             app.UseAuthorization();
+
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
